c39b351c6e739e1dd6c25ec869713fc2ced085b5
carlos <carlosm@gmail.com>
2024-02-29 12:08:24 +0100

    cambio de long a BigInteger

--- factorial.java
+++ factorial.java
@@ -1,28 +1,30 @@
 import java.util.Scanner;
+import java.math.BigInteger;
 
-public class getNumber() {
-    Scanner scanInput = new Scanner(System.in);
-    System.out.print("Introduzca un numero: ");
-    long num = scanInput.nextLong();
-    scanInput.close();
-    return num;
+public static BigInteger fact(BigInteger num){
+    if (num.compareTo(BigInteger.ONE)<=0){
+        return BigInteger.ONE;
+    } else {
+        return num.multiply(fact(num.subtract(BigInteger.ONE)));
     }
-
-
-
+}
 
 
     public static void main(String [] args){
-        long num = getNumber();
+        Scanner scanInput = new Scanner(System.in);
+        System.out.print("Introduzca un numero: ");
+        BigInteger num = BigInteger.valueOf(scanInput.nextLong());
         
-        if (num<0) {
+        if (num.compareTo(BigInteger.ZERO)<0) {
             System.out.println("El factorial no esta definido" + " para numeros negativos");
         
-        
         } else {
-            long fac = fact(num);
-            
+            BigInteger fac = fact(num);
+
             System.out.println("El factorial de "+num+" es "+fac);
         }
         
+        scanInput.close();
+        
     }
+
